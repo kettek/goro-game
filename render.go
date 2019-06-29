affaa68f58aa29558dea5332693087f39e88055f
kts of kettek (nyaa) <kettek1@kettek.net>
2019-06-28 19:11:34 -0700

    Add comments

--- render.go
+++ render.go
@@ -27,16 +27,19 @@ func DrawAll(screen *goro.Screen, entities []*entity.Entity, gameMap mapping.Gam
 	screen.Flush()
 }
 
+// ClearAll clears all entities from the screen.
 func ClearAll(screen *goro.Screen, entities []*entity.Entity) {
 	for _, entity := range entities {
 		ClearEntity(screen, entity)
 	}
 }
 
+// DrawEntity draws a given entity to the screen.
 func DrawEntity(screen *goro.Screen, e *entity.Entity) {
 	screen.DrawRune(e.X, e.Y, e.Rune, e.Style)
 }
 
+// ClearEntity clears a given entity from the screen.
 func ClearEntity(screen *goro.Screen, e *entity.Entity) {
 	screen.DrawRune(e.X, e.Y, ' ', goro.Style{})
 }
